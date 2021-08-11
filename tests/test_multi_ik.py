403149aea7216efe9572d5c82c0653220c917752
mjd3 <michael.danielczuk@gmail.com>
2021-08-10 19:25:30 -0700

    add basic multiprocessing IK implementation + tests (#6)
    * add basic multiprocessing IK implementation + tests
    
    * formatting

---
+++ tests/test_multi_ik.py
@@ -0,0 +1,210 @@
+import numpy as np
+import os
+import pytest
+
+from tracikpy import MultiTracIKSolver
+
+
+@pytest.fixture
+def ik_solver(num_workers):
+    if num_workers is not None:
+        return MultiTracIKSolver(
+            "data/franka_panda.urdf",
+            "panda_link0",
+            "panda_hand",
+            timeout=0.05,
+            num_workers=num_workers,
+        )
+    else:
+        return MultiTracIKSolver(
+            "data/franka_panda.urdf", "panda_link0", "panda_hand", timeout=0.05
+        )
+
+
+@pytest.fixture
+def ee_pose():
+    return np.array(
+        [
+            [0.0525767, -0.64690764, -0.7607537, 0.0],
+            [-0.90099786, -0.35923817, 0.24320937, 0.2],
+            [-0.43062577, 0.67265031, -0.60174996, 0.4],
+            [0.0, 0.0, 0.0, 1.0],
+        ]
+    )
+
+
+# Test initialization
+@pytest.mark.parametrize("num_workers", [None, 2])
+def test_init(ik_solver):
+    assert len(ik_solver.ik_procs) == ik_solver.num_workers
+    assert ik_solver.number_of_joints == 7
+    assert ik_solver.joint_names == (
+        "panda_joint1",
+        "panda_joint2",
+        "panda_joint3",
+        "panda_joint4",
+        "panda_joint5",
+        "panda_joint6",
+        "panda_joint7",
+    )
+    assert ik_solver.link_names == (
+        "panda_link1",
+        "panda_link2",
+        "panda_link3",
+        "panda_link4",
+        "panda_link5",
+        "panda_link6",
+        "panda_link7",
+        "panda_hand",
+    )
+
+
+# Test bad initialization
+def test_bad_init():
+    with pytest.raises(ValueError):
+        MultiTracIKSolver(
+            "data/franka_panda.urdf",
+            "panda_link0",
+            "panda_hand",
+            timeout=0.05,
+            num_workers=2.5,
+        )
+
+    with pytest.raises(ValueError):
+        MultiTracIKSolver(
+            "data/franka_panda.urdf",
+            "panda_link0",
+            "panda_hand",
+            timeout=0.05,
+            num_workers=0,
+        )
+
+    with pytest.raises(ValueError):
+        MultiTracIKSolver(
+            "data/franka_panda.urdf",
+            "panda_link0",
+            "panda_hand",
+            timeout=0.05,
+            num_workers=os.cpu_count() + 1,
+        )
+
+
+# Test getting and setting joint limits
+@pytest.mark.parametrize("num_workers", [None])
+def test_joint_limits(ik_solver):
+    assert np.allclose(
+        ik_solver.joint_limits[0],
+        np.array(
+            [
+                -2.8973,
+                -1.76279998,
+                -2.8973,
+                -3.07179999,
+                -2.8973,
+                -0.0175,
+                -2.8973,
+            ]
+        ),
+    )
+    assert np.allclose(
+        ik_solver.joint_limits[1],
+        np.array(
+            [2.8973, 1.76279998, 2.8973, -0.0698, 2.8973, 3.75250006, 2.8973]
+        ),
+    )
+
+    rg = np.random.default_rng()
+    new_limits = (
+        rg.standard_normal(ik_solver.number_of_joints),
+        rg.standard_normal(ik_solver.number_of_joints),
+    )
+    ik_solver.joint_limits = new_limits
+    assert np.allclose(ik_solver.joint_limits[0], new_limits[0])
+    assert np.allclose(ik_solver.joint_limits[1], new_limits[1])
+
+
+# Test solving FK in parallel
+@pytest.mark.parametrize("num_workers", [None])
+def test_multi_fk(ik_solver):
+    rand_qs = np.random.default_rng().uniform(
+        *ik_solver.joint_limits, size=(100, ik_solver.number_of_joints)
+    )
+    output_fks = ik_solver.fk(rand_qs)
+    assert output_fks.shape == (100, 4, 4)
+    output_fks = ik_solver.fk(rand_qs.tolist())
+    assert output_fks.shape == (100, 4, 4)
+
+
+@pytest.mark.parametrize("num_workers", [None])
+def test_multi_ik(ik_solver, ee_pose):
+    # Test solving IK in parallel without qinit
+    qout = ik_solver.ik(ee_pose, num_seeds=100)
+    assert qout.shape == (ik_solver.number_of_joints,)
+    assert not np.isnan(qout).any()
+
+    # Test solving IK in parallel with qinit
+    qout = ik_solver.ik(
+        ee_pose, qinit=np.zeros(ik_solver.number_of_joints), num_seeds=100
+    )
+    assert not np.isnan(qout).any()
+    assert qout.shape == (ik_solver.number_of_joints,)
+
+    # Test case where no solution is available (unreachable pose)
+    bad_ee_pose = ee_pose.copy()
+    bad_ee_pose[2, 3] += 2
+    qout = ik_solver.ik(
+        bad_ee_pose, qinit=np.zeros(ik_solver.number_of_joints)
+    )
+    assert np.isnan(qout).all()
+
+
+@pytest.mark.parametrize("num_workers", [None])
+def test_multi_iks(ik_solver, ee_pose):
+    # Test iks with no qinit
+    qouts = ik_solver.iks(
+        np.repeat(ee_pose[None, ...], 100, axis=0), num_seeds=100
+    )
+    assert qouts.shape == (100, ik_solver.number_of_joints)
+    assert not np.isnan(qouts).any()
+
+    # Test iks with qinit
+    qouts = ik_solver.iks(
+        np.repeat(ee_pose[None, ...], 100, axis=0),
+        qinits=np.zeros((100, ik_solver.number_of_joints)),
+        num_seeds=100,
+    )
+    assert qouts.shape == (100, ik_solver.number_of_joints)
+    assert not np.isnan(qouts).any()
+
+
+# Test that exceptions are raised correctly for bad inputs
+@pytest.mark.parametrize("num_workers", [None])
+def test_bad_inputs(ik_solver, ee_pose):
+    bad_qinit = np.zeros(ik_solver.number_of_joints - 1)
+    with pytest.raises(ValueError):
+        ik_solver.ik(ee_pose, qinit=bad_qinit)
+
+    with pytest.raises(ValueError):
+        ik_solver.ik(ee_pose[:3, :3])
+
+    with pytest.raises(ValueError):
+        ik_solver.iks(ee_pose[None, :3, :3])
+
+    with pytest.raises(ValueError):
+        ik_solver.iks(
+            np.repeat(ee_pose[None, ...], 100, axis=0),
+            np.zeros((99, ik_solver.number_of_joints)),
+        )
+
+    with pytest.raises(ValueError):
+        ik_solver.fk(bad_qinit)
+
+    bounds = ik_solver.joint_limits[0]
+    with pytest.raises(ValueError):
+        ik_solver.joint_limits = bounds
+
+    with pytest.raises(ValueError):
+        ik_solver.joint_limits = (bad_qinit, bounds)
+
+    with pytest.raises(ValueError):
+        ik_solver.joint_limits = (bounds, bad_qinit)
---
+++ tracikpy/multi_ik_solver.py
@@ -0,0 +1,247 @@
+import multiprocessing as mp
+import numpy as np
+import os
+import queue
+
+from .trac_ik_solver import TracIKSolver
+
+
+class TracIKProc(mp.Process):
+    """
+    Used for finding ik in parallel.
+    """
+
+    def __init__(
+        self,
+        output_queue,
+        urdf_file,
+        base_link,
+        tip_link,
+        timeout=0.005,
+        epsilon=1e-5,
+        solve_type="Speed",
+    ):
+        super().__init__()
+        self.output_queue = output_queue
+        self.input_queue = mp.Queue()
+        self.ik_solver = TracIKSolver(
+            urdf_file,
+            base_link,
+            tip_link,
+            timeout,
+            epsilon,
+            solve_type,
+        )
+
+    def _ik(self, ee_pose, qinit, bx, by, bz, brx, bry, brz):
+        return self.ik_solver.ik(ee_pose, qinit, bx, by, bz, brx, bry, brz)
+
+    def _fk(self, q):
+        return self.ik_solver.fk(q)
+
+    def run(self):
+        while True:
+            try:
+                request = self.input_queue.get(timeout=1)
+            except queue.Empty:
+                continue
+            ret = getattr(self, "_" + request[0])(*request[1:-1])
+            self.output_queue.put((request[-1], ret))
+
+    def ik(
+        self,
+        grasp,
+        qinit=None,
+        bx=1e-5,
+        by=1e-5,
+        bz=1e-5,
+        brx=1e-3,
+        bry=1e-3,
+        brz=1e-3,
+        ind=None,
+    ):
+        self.input_queue.put(
+            ("ik", grasp, qinit, bx, by, bz, brx, bry, brz, ind)
+        )
+
+    def fk(self, q, ind=None):
+        self.input_queue.put(("fk", q, ind))
+
+
+class MultiTracIKSolver:
+    def __init__(
+        self,
+        urdf_file,
+        base_link,
+        tip_link,
+        timeout=0.005,
+        epsilon=1e-5,
+        solve_type="Speed",
+        num_workers=os.cpu_count(),
+    ):
+        self.output_queue = mp.Queue()
+        self.num_workers = num_workers
+        self.ik_procs = []
+        if (
+            not isinstance(self.num_workers, int)
+            or self.num_workers <= 0
+            or self.num_workers > os.cpu_count()
+        ):
+            raise ValueError(
+                "num_workers must be an integer between "
+                f"1 and {os.cpu_count()}!"
+            )
+        for _ in range(num_workers):
+            self.ik_procs.append(
+                TracIKProc(
+                    self.output_queue,
+                    urdf_file,
+                    base_link,
+                    tip_link,
+                    timeout,
+                    epsilon,
+                    solve_type,
+                )
+            )
+            self.ik_procs[-1].daemon = True
+            self.ik_procs[-1].start()
+
+    @property
+    def joint_limits(self):
+        """
+        Return lower bound limits and upper bound limits for all the joints
+        in the order of the joint names.
+        """
+        return self.ik_procs[0].ik_solver.joint_limits
+
+    @joint_limits.setter
+    def joint_limits(self, bounds):
+        try:
+            lower_bounds, upper_bounds = bounds
+        except ValueError:
+            raise ValueError("bounds must be an iterable with two lists")
+        if len(lower_bounds) != self.number_of_joints:
+            raise ValueError(
+                "lower_bounds array size mismatch, input size "
+                f"{len(lower_bounds):d}, should be {self.number_of_joints:d}"
+            )
+
+        if len(upper_bounds) != self.number_of_joints:
+            raise ValueError(
+                "upper_bounds array size mismatch, input size "
+                f"{len(upper_bounds):d}, should be {self.number_of_joints:d}"
+            )
+
+        for ikp in self.ik_procs:
+            ikp.ik_solver.joint_limits = bounds
+
+    @property
+    def number_of_joints(self):
+        return self.ik_procs[0].ik_solver.number_of_joints
+
+    @property
+    def joint_names(self):
+        return self.ik_procs[0].ik_solver.joint_names
+
+    @property
+    def link_names(self):
+        return self.ik_procs[0].ik_solver.link_names
+
+    # Calculates FK for a vector of cfgs
+    # (NOTE: this should be vectorized on C++ side)
+    def fk(self, q):
+        if not isinstance(q, np.ndarray):
+            q = np.asarray(q, dtype=np.float64)
+        if q.ndim == 1:
+            q = q[None, :]
+        if q.shape[1] != self.number_of_joints:
+            raise ValueError(
+                f"q must be of shape (n, {self.number_of_joints})!"
+            )
+
+        # Calculate FK for all cfgs distributed between processes
+        for i, qi in enumerate(q):
+            self.ik_procs[i % self.num_workers].fk(qi, ind=i)
+
+        # collect computed fks
+        fks = np.zeros((len(q), 4, 4))
+        for _ in range(len(q)):
+            output = self.output_queue.get(True)
+            if output[1] is None:
+                continue
+            fks[output[0]] = output[1]
+
+        return fks
+
+    # Find ik for a single ee_pose and multiple seeds distributed
+    # between processes. Returns the closest to qinit if specified;
+    # otherwise the first found
+    def ik(self, ee_pose, qinit=None, num_seeds=1):
+        if not isinstance(ee_pose, np.ndarray) or ee_pose.shape != (4, 4):
+            raise ValueError("ee_pose must be numpy array of shape (4, 4)!")
+        if qinit is not None and (
+            not isinstance(qinit, np.ndarray)
+            or qinit.shape != (self.number_of_joints,)
+        ):
+            raise ValueError(
+                "qinit must either be None or numpy array of "
+                f"shape ({self.number_of_joints},)!"
+            )
+
+        self.ik_procs[0].ik(ee_pose, qinit, ind=0)
+        for i in range(num_seeds - 1):
+            self.ik_procs[(i + 1) % self.num_workers].ik(
+                ee_pose, qinit=qinit, ind=i
+            )
+
+        # collect computed iks
+        final_ik_inds = []
+        final_ik = []
+        for _ in range(num_seeds):
+            output = self.output_queue.get(True)
+            if output[1] is None:
+                continue
+            final_ik_inds.append(output[0])
+            final_ik.append(output[1])
+
+        if len(final_ik) == 0:
+            final_ik = np.empty(self.number_of_joints)
+            final_ik.fill(np.nan)
+            return final_ik
+        elif qinit is None:
+            return final_ik[0]
+        elif 0 in final_ik_inds:
+            return final_ik[final_ik_inds.index(0)]
+        else:
+            closest_ind = np.argmin(
+                np.linalg.norm(np.array(final_ik) - qinit, axis=1)
+            )
+            return final_ik[closest_ind]
+
+    # Finds ik for many ee_pose, qinit pairs
+    def iks(self, ee_poses, qinits=None, num_seeds=1):
+        if (
+            not isinstance(ee_poses, np.ndarray)
+            or ee_poses.ndim != 3
+            or ee_poses.shape[1] != 4
+            or ee_poses.shape[2] != 4
+        ):
+            raise ValueError(
+                "ee_poses must be a numpy array of shape (n, 4, 4)!"
+            )
+        if qinits is None:
+            qinits = [None] * len(ee_poses)
+        elif (
+            not isinstance(qinits, np.ndarray)
+            or qinits.ndim != 2
+            or qinits.shape != (len(ee_poses), self.number_of_joints)
+        ):
+            raise ValueError(
+                "qinits must be a 2D numpy array of same length as ee_poses!"
+            )
+
+        iks = np.empty((len(ee_poses), self.number_of_joints))
+        for i, (ee_pose, qinit) in enumerate(zip(ee_poses, qinits)):
+            iks[i] = self.ik(ee_pose, qinit=qinit, num_seeds=num_seeds)
+
+        return iks
